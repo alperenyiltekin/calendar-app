4456c6a6e49aae81c2daefbfb863f4317d8012e0
alperenyiltekin <fatihalperen.yiltekin@gmail.com>
2021-08-14 00:53:28 +0300

    Finished

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
-# vue-calendar
+# Vue Calendar App
+Calendar app that you can adding and deleting events.
+Don't forget the setting up your firebase config values in `main.js`
+## Techs
+Vue, Vuetify, Firebase
 
-## Project setup
-```
-npm install
-```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Demo
+![](./src/assets/events.jpg)
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
+import 'firebase/firestore'
+import VueTextAreaAutoSize from 'vue-textarea-autosize'
+
+Vue.use(firebase)
+Vue.use(VueTextAreaAutoSize)
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+});
+export const db = firebase.firestore()
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  VueTextAreaAutoSize,
+  render: h => h(App)
 }).$mount('#app')
